1ece0b4311724c5109475667df82778d0cca9312
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-12-17 08:56:01 +0000

    Add set

---
+++ set.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var car = {};
+
+var property;
+
+Object.defineProperty(car, 'name', {
+  set: function (value) {
+    var nameParts = value.split(' ');
+
+    this.make = nameParts[0];
+    this.model = nameParts[1] + ' ' + nameParts[2];
+  }
+});
+
+car.name = 'Tesla Model X';
+
+console.log(car.make);
+console.log(car.model);
